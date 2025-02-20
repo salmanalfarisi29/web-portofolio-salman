01e9734409a58ddc1cb5e6a75533cab9924c7ebb
Salman Alfarisi <95122631+salmanalfarisi29@users.noreply.github.com>
2025-02-20 20:22:38 +0700

    FIX : BUG HEADER

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
 
   return (
     <header className="fixed top-0 left-0 w-full z-50 bg-white dark:bg-gray-900 shadow-md py-4 transition-all duration-300">
-      <div className="container mx-auto px-6 flex justify-between items-center">
+      <div className="container mx-auto px-6 flex justify-between items-center w-full">
         {/* Branding */}
         <h1 className="text-xl font-bold text-gray-800 dark:text-white">
           Salman&apos;s Portfolio
